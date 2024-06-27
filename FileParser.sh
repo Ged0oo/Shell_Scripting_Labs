bf3dff0308aa74260c049964798984ab7c596855
Ged0oo <mohmmednagy@gmail.com>
2024-06-27 03:07:07 +0300

    solve bug in file parser task

--- FileParser.sh
+++ FileParser.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 file=text.txt
+if [ -f ${file} ]
+then 
+    while read line
+    do    
+        if [ -z "${line}" ]
+        then    
+            echo "End of File"
 
-while read line
-do    
-    if [ -z "${line}" ]
-    then    
-        echo "End of File"
+        elif [ "${line}" = Ged0oo ]
+        then
+            echo "=== Hello Ged0oo ==="
+            
+        else
+            echo ${line}
+        fi
 
-    elif [ "${line}" = Ged0oo ]
-    then
-        echo "=== Hello Ged0oo ==="
-        
-    else
-        echo ${line}
-    fi
-
-done < ${file}
+    done < ${file}
+fi
